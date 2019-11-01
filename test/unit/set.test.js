bf56b9a5df6cb5a802215e1731f184d0c9832127
Sascha Goldhofer <post@saschagoldhofer.de>
2019-11-01 14:10:45 +0100

    Update set array-item behaviour

--- test/unit/set.test.js
+++ test/unit/set.test.js
@@ -2,7 +2,14 @@
 const { expect } = require("chai");
 const set = require("../../lib/set");
 
+/*
+    ??? Syntax
 
+    - create array    /path/[]/property     -> path[{ property }], path[..., { property }]
+    - insert array    /path/[1]/property    -> path[0, { property }, 1, ...]
+    - replace array   /path/1/property      -> path[0, { property }, 2, ...]
+        -> tests for index: isNaN(parseInt(property)) === false
+ */
 describe("set", () => {
     // should return data on empty string
     // should return value for root pointer
@@ -29,21 +36,41 @@ describe("set", () => {
         expect(result).to.deep.eq({ outer: { side: true, inner: { value: "#/outer/inner/value" } }});
     });
 
+    it("should create array for number", () => {
+        const result = set({}, "/path/0/value", 9);
+        expect(result).to.deep.eq({ path: [{ value: 9 }] });
+    });
+
+    it("should create object for escaped number", () => {
+        const result = set({}, "/path/\"0\"/value", 9);
+        expect(result).to.deep.eq({ path: { 0: { value: 9 } } });
+    });
+
+    it("should create array for number target", () => {
+        const result = set({ outer: { side: true } }, "/outer/inner/0", 9);
+        expect(result).to.deep.eq({ outer: { side: true, inner: [9] }});
+    });
+
+    it("should create array for escaped number target", () => {
+        const result = set({ outer: { side: true } }, "/outer/inner/\"0\"", 9);
+        expect(result).to.deep.eq({ outer: { side: true, inner: { 0: 9 } }});
+    });
+
 
     describe("array", () => {
 
-        it.only("should replace array item at specified position", () => {
+        it("should replace array item at specified position", () => {
             const result = set({ list: [1,2,3] }, "/list/1", "t");
-            expect(result).to.deep.eq({ list: [1,"t",3] });
+            expect(result).to.deep.eq({ list: [1, "t" , 3] });
         });
 
         it("should create array and append item", () => {
-            const result = set({}, "/outer[]/[]/value", 9);
+            const result = set({}, "/outer/[]/value", 9);
             expect(result).to.deep.eq({ outer: [{ value: 9 }] });
         });
 
-        it("should create array based on array indicator", () => {
-            const result = set({}, "/outer[]/1/value", 9);
+        it("should create array based on property-type", () => {
+            const result = set({}, "/outer/1/value", 9);
             expect(result).to.deep.eq({ outer: [undefined, { value: 9 }] });
         });
 
