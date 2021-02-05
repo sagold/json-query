80f29a9a11642c7e8e1ca54ae3fd458798df88ad
Sascha Goldhofer <post@saschagoldhofer.de>
2021-02-05 17:32:14 +0100

    add: expose types

--- lib/index.ts
+++ lib/index.ts
@@ -2,3 +2,4 @@ export { default as get } from "./get";
 export { default as set } from "./set";
 export { default as split } from "./split";
 export { default as remove } from "./remove";
+export type { Input, JSONPointer, QueryResult } from "./types";
