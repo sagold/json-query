34becc24ed769d02739251639d7b10fe25fe0d19
Sascha Goldhofer <post@saschagoldhofer.de>
2021-02-07 13:14:57 +0100

    add: export result and query types

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
-import get from "./lib/get";
+import get, { ReturnType, ResultCallback } from "./lib/get";
 import set from "./lib/set";
 import split from "./lib/split";
 import remove from "./lib/remove";
 
-export { get, set, split, remove };
-export default { get, set, split, remove };
+export { get, set, split, remove, ReturnType };
+export default { get, set, split, remove, ReturnType };
+
+export type { ResultCallback };
 export type { Input, JSONPointer, QueryResult } from "./lib/types";
